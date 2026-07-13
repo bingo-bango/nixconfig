647feceea8ade081dd3e300023822f8fcf8ee8a4
Eric Cheng <eric.cheng@cerebras.net>
2026-07-13 14:35:47 -0400

    refactor: remove acme from vars.nix

--- vars.nix
+++ vars.nix
@@ -4,7 +4,6 @@
   userName = "eh8";
   domain = "chengeric.com";
   userEmail = "eric@chengeric.com";
-  acmeEmail = "admin+acme@chengeric.com";
   timeZone = "America/New_York";
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkcgwjYMHqUDnx0JIOSXQ/TN80KEaFvvUWA2qH1AHFC";
   sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7Ju7wpJpiW0D7kq3u9NMJ6M2L0q/IRhA5tNOaAnSdj";
