ab41112910238d81d8cdea1ff79afbd56e00300e
Eric Cheng <eric@chengeric.com>
2026-08-12 02:42:08 -0400

    fix: more remote unlock stuff

--- modules/nixos/remote-unlock.nix
+++ modules/nixos/remote-unlock.nix
@@ -6,13 +6,14 @@
   boot = {
     kernelParams = ["ip=dhcp"];
     initrd = {
-      luks.reusePassphrases = true;
-      systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
       network = {
         enable = true;
         ssh = {
           enable = true;
-          authorizedKeys = config.users.users.${vars.userName}.openssh.authorizedKeys.keys;
+          authorizedKeys =
+            map
+            (key: ''command="systemctl default" ${key}'')
+            config.users.users.${vars.userName}.openssh.authorizedKeys.keys;
           hostKeys = ["/nix/secret/initrd/ssh_host_ed25519_key"];
         };
       };
