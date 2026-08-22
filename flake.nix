55b9d5660d6d388e1ae38728fa2030fe1f15304e
Peter Cairns <peter@cairns.cc>
2026-08-22 14:44:45 +0200

    snafu with flake.nix not up dated

--- flake.nix
+++ flake.nix
@@ -4,9 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: let
+  outputs = { self, nixpkgs, sops-nix, ... }@inputs: let
     vars = import ./vars.nix;
 
     mkNixOSConfig = path:
